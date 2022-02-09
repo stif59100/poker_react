11d97bc549c90f72b127383d1a8d9c7895b56599
Steeve Vanderstocken <steeve.vanderstocken@gmail.com>
2022-02-09 22:41:26 +0100

    09/02

--- src/Page/Authentication/Authentication.js
+++ src/Page/Authentication/Authentication.js
@@ -6,6 +6,8 @@ import { Redirect } from "react-router-dom/cjs/react-router-dom.min";
 import { observer } from "mobx-react-lite";
 
 
+
+
 const ErrorsForm = (ErrorsForm) =>
   ErrorsForm.length > 0
     ?
@@ -23,6 +25,7 @@ const Forms = observer((props) => {
   const [email, setEmail] = useState("")
   const [password, setPassword] = useState("")
   const [errorsForm, setErrorsForm] = useState([]);
+  const [passwordIsVisible, setPasswordIsVisible] = useState(false)
   const onChangeEmail = (event) => setEmail(event.target.value)
   const onPassword = (event) => setPassword(event.target.value)
   const onSubmit = (event) => {
@@ -32,7 +35,14 @@ const Forms = observer((props) => {
         console.log("notlogged")
         props.Profile.fetchGetProfile(email, password);
       }
+      
   }
+  const checkPasswordVisible = (event)=>{
+    console.log('checkbox')
+    setPasswordIsVisible(!passwordIsVisible)
+  }
+
+    
   return (
     <section className="row authentication" >
       <div className="col-12 col-lg-6 offset-lg-3">
@@ -43,9 +53,15 @@ const Forms = observer((props) => {
             <input id="inputEmail" className="form-control input-grey-light" type="text" aria-describedby="emailHelp" placeholder="Votre adresse email ou pseudo" onChange={onChangeEmail} value={email} />
           </div>
           <div className="form-group">
-            <label htmlFor="inputPassword" className="w-100 text-left color-gold-light">Mot de passe</label>
-            <input className="form-control input-grey-light" id="inputPassword" type="password" placeholder="Votre mot de passe" onChange={onPassword} value={password} />
-          </div>
+            <label htmlFor="inputPassword" className="w-100 text-left color-gold-light" id='mdp'>Mot de passe</label>
+            {(passwordIsVisible)?
+            <input className="form-control input-grey-light" id="InputNotVisible" type="password" required placeholder="Votre mot de passe" onChange={onPassword} value={password} />
+            :
+            <input className="form-control input-grey-light" id="inputVisible" type="text" required placeholder="Votre mot de passe" onChange={onPassword} value={password} />}
+            <label for="chkVisibility" id='lblVisible'>Mot de passe visible</label>
+            <input type="checkbox" id='chkVisibilite' onChange={checkPasswordVisible} />
+            <br></br>
+            </div>
           <button type="submit" className="btn btn-gold-light">valider</button>
         </form>
         <Link to='/resetPassword' className="color-gold-light">Identifiant ou mot de passe oublié?</Link>
